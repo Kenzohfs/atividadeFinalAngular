7e9b86dc1df706a7faedbe9ad109e59d3b658f32
Kenzohfs <88584837+Kenzohfs@users.noreply.github.com>
2022-02-07 21:55:33 -0300

    update

--- gameStore/src/app/login/login.module.ts
+++ gameStore/src/app/login/login.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-
-const routes: Routes = [
-  {
-    
-  }
-]
 
 @NgModule({
   imports: [
